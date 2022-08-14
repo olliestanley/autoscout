8708020e087ae398dad374c80b629f3fd87a1707
Oliver Stanley <o.dzineit@gmail.com>
2022-08-14 20:51:28 +0100

    Begin creation of automatically selected positional radars

--- autoscout/vis/constant.py
+++ autoscout/vis/constant.py
@@ -1,6 +1,50 @@
 RADAR_COLOURS = [
-    "#ffb2b2",
-    "#fc5f5f",
-    "#1BDAD2",
-    "#D5D527",
+    "#FFFFFF",
+    "#C5BFBF",
+    "#DF1414",
+    "#000000",
+]
+
+MIDFIELD_COLUMNS = [
+    "passes_pct",
+    "passes_into_final_third",
+    "carries_into_final_third",
+    "xa_per90",
+    "dribbles_completed",
+    "fouled",
+    "dispossessed",
+    "pressure_regains",
+    "tackles_won",
+    "pressures",
+    "interceptions",
+]
+
+MIDFIELD_DISPLAY_COLUMNS = [
+    "Pass Completion",
+    "Passes into Final Third",
+    "Carries into Final Third",
+    "xG Assisted",
+    "Dribbles Completed",
+    "Fouls Won",
+    "Turnovers",
+    "Pressure Regains",
+    "Pressures",
+    "Tackles Won",
+    "Interceptions",
+]
+
+MIDFIELD_NORMALIZE_COLUMNS = [
+    "passes_into_final_third",
+    "carries_into_final_third",
+    "dribbles_completed",
+    "fouled",
+    "dispossessed",
+    "pressure_regains",
+    "tackles_won",
+    "pressures",
+    "interceptions",
+]
+
+MIDFIELD_LOWER_IS_BETTER = [
+    "dispossessed"
 ]
--- autoscout/vis/radar.py
+++ autoscout/vis/radar.py
@@ -4,7 +4,27 @@ import matplotlib as mpl
 import pandas as pd
 from mplsoccer import Radar
 
+from autoscout.preprocess import tabular
 from autoscout.vis import constant
+
+
+def plot_midfield_radar(
+    data: pd.DataFrame,
+    index: Union[str, int],
+    adjust_per_90: bool = True,
+    **kwargs,
+) -> Tuple[Radar, mpl.figure.Figure, mpl.axes.Axes]:
+    if adjust_per_90:
+        data = tabular.adjust_per_90(data, constant.MIDFIELD_NORMALIZE_COLUMNS)
+
+    return plot_radar(
+        data,
+        constant.MIDFIELD_COLUMNS,
+        index,
+        constant.MIDFIELD_DISPLAY_COLUMNS,
+        constant.MIDFIELD_LOWER_IS_BETTER,
+        **kwargs,
+    )
 
 
 def plot_radar(
@@ -12,17 +32,24 @@ def plot_radar(
     columns: Sequence[str],
     index: Union[str, int],
     columns_display: Sequence[str] = None,
+    lower_is_better: Sequence[str] = None,
+    **kwargs,
 ) -> Tuple[Radar, mpl.figure.Figure, mpl.axes.Axes]:
     if columns_display:
         mapper = dict(zip(columns, columns_display))
         data = data.rename(mapper, axis=1)
         columns = columns_display
 
+        if lower_is_better:
+            lower_is_better = [mapper[v] for v in lower_is_better]
+
     # Set radar bounds based on quantiles of all rows in the DataFrame
     radar = Radar(
         params=columns,
         min_range=data[columns].quantile(0.05),
         max_range=data[columns].quantile(0.95),
+        lower_is_better=lower_is_better,
+        **kwargs,
     )
 
     # Now quantiles are calculated, extract the desired row
@@ -35,7 +62,7 @@ def plot_radar(
     # Keep only the columns we want to plot
     values: pd.Series = data[columns].squeeze()
 
-    fig, ax = radar.setup_axis()
+    fig, ax = radar.setup_axis(figsize=(10, 10))
 
     radar.draw_circles(
         ax=ax, facecolor=constant.RADAR_COLOURS[0], edgecolor=constant.RADAR_COLOURS[1]
@@ -43,10 +70,10 @@ def plot_radar(
     radar.draw_radar(
         values,
         ax=ax,
-        kwargs_radar={"facecolor": constant.RADAR_COLOURS[2], "alpha": 0.9},
-        kwargs_rings={"facecolor": constant.RADAR_COLOURS[3], "alpha": 0.7},
+        kwargs_radar={"facecolor": constant.RADAR_COLOURS[2], "alpha": 0.5},
+        kwargs_rings={"facecolor": constant.RADAR_COLOURS[3], "alpha": 0.5},
     )
     radar.draw_range_labels(ax=ax)
-    radar.draw_param_labels(ax=ax)
+    radar.draw_param_labels(ax=ax, offset=0.5)
 
     return radar, fig, ax
