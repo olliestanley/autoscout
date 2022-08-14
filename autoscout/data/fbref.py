dcf16c39f407f1818032c7cd744ce78bd4d27d66
Oliver Stanley <o.dzineit@gmail.com>
2022-08-14 15:44:08 +0100

    Fix downloading team data

--- autoscout/data/fbref.py
+++ autoscout/data/fbref.py
@@ -14,7 +14,7 @@ def get_data(
     end: str,
     team: bool = False,
     vs: bool = False,
-    sleep_seconds: float = 10.0,
+    sleep_seconds: float = 5.0,
 ) -> pd.DataFrame:
     df = pd.concat([
         sleep_and_return(
@@ -76,7 +76,7 @@ def get_df_team(
     features: Sequence[str],
     team_table
 ) -> pd.DataFrame:
-    pre_df_squad = dict()
+    pre_df_team = dict()
     # features does not contain squad name, needs special treatment
     rows = team_table.find_all("tr")
 
@@ -85,14 +85,14 @@ def get_df_team(
             continue
 
         name = (
-            row.find("th", {"data-stat": "squad"})
+            row.find("th", {"data-stat": "team"})
             .text.strip().encode().decode("utf-8")
         )
 
-        if "squad" in pre_df_squad:
-            pre_df_squad["squad"].append(name)
+        if "team" in pre_df_team:
+            pre_df_team["team"].append(name)
         else:
-            pre_df_squad["squad"] = [name]
+            pre_df_team["team"] = [name]
 
         for feat in features:
             cell = row.find("td", {"data-stat": feat})
@@ -103,12 +103,12 @@ def get_df_team(
             if feat not in ("player", "nationality", "position", "team", "age", "birth_year"):
                 text = float(text.replace(",", ""))
 
-            if feat in pre_df_squad:
-                pre_df_squad[feat].append(text)
+            if feat in pre_df_team:
+                pre_df_team[feat].append(text)
             else:
-                pre_df_squad[feat] = [text]
+                pre_df_team[feat] = [text]
 
-    return pd.DataFrame.from_dict(pre_df_squad)
+    return pd.DataFrame.from_dict(pre_df_team)
 
 
 def get_tables(url: str, vs: bool = False) -> Tuple:
