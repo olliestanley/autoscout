ac0e3fc618bb66e7e36a4e6ff9040d78a4be5fc9
Oliver Stanley <o.dzineit@gmail.com>
2022-08-14 15:37:41 +0100

    Add support for non-current seasons to fbref downloads

--- scripts/data/download_fbref.py
+++ scripts/data/download_fbref.py
@@ -10,7 +10,8 @@ from autoscout.data import fbref, util
 
 
 def download_player_data_for_comp(
-    comp_config: Dict[str, str],
+    url_top: str,
+    url_end: str,
     stats_config: Dict[str, Dict[str, Sequence[str]]],
     out_dir: Union[str, Path] = "data/fbref",
     keeper: bool = False,
@@ -19,23 +20,24 @@ def download_player_data_for_comp(
 
     df = fbref.get_data(
         stats_config[keeper_str],
-        comp_config["top"],
-        comp_config["end"],
+        url_top,
+        url_end,
     )
 
     util.write_dated_csv(df, out_dir, keeper_str, index=False)
 
 
 def download_team_data_for_comp(
-    comp_config: Dict[str, str],
+    url_top: str,
+    url_end: str,
     stats_config: Dict[str, Dict[str, Sequence[str]]],
     out_dir: Union[str, Path] = "data/fbref",
     vs: bool = False,
 ) -> None:
     df = fbref.get_data(
         stats_config["team"],
-        comp_config["top"],
-        comp_config["end"],
+        url_top,
+        url_end,
         team=True,
         vs=vs,
     )
@@ -51,11 +53,22 @@ if __name__ == "__main__":
     parser.add_argument("--competition", "--comp", type=str, default="pl")
     parser.add_argument("--type", type=str, default="outfield")
     parser.add_argument("--vs", action="store_true")
+    parser.add_argument("--season", type=str, default="current")
     args = parser.parse_args()
 
     config_dir = Path(args.config)
     comps_json = util.load_json(config_dir / "comps.json")
     stats_json = util.load_json(config_dir / "stats.json")
+
+    comp: Dict[str, str] = comps_json[args.competition]
+    url_top, url_end = comp["top"], comp["end"]
+
+    if args.season.isdigit():
+        season = int(args.season)
+        season_str = f"{season - 1}-{season}"
+
+        url_top += f"{season_str}/"
+        url_end = f"/{season_str}-{url_end[1:]}"
 
     if args.type == "team":
         download_team_data_for_comp(
