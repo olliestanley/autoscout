1dedea95d21e7af94096c00a609b0162eaf3fb9a
Oliver Stanley <o.dzineit@gmail.com>
2022-08-14 15:11:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='autoscout',
+   version='0.1.0',
+   description='Autoscout for football',
+   author='Oliver Stanley',
+   packages=['autoscout'],
+   install_requires=['wheel'],
+)
