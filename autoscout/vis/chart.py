4fb8c79d45d331af11eb5b9099835c78aa8d55fb
Oliver Stanley <o.dzineit@gmail.com>
2022-08-20 13:44:29 +0100

    seaborn -> bokeh for chart

--- autoscout/vis/chart.py
+++ autoscout/vis/chart.py
@@ -1,49 +1,66 @@
-from typing import Any, Dict, List, Tuple, Union
-
 import pandas as pd
-import seaborn as sns
-from matplotlib.colors import Colormap
+from bokeh.models import ColumnDataSource, HoverTool, LabelSet
+from bokeh.plotting import figure
+from bokeh.plotting.figure import Figure
 
 
 def scatter_with_labels(
     data: pd.DataFrame,
     x: str,
     y: str,
-    hue: str = None,
-    palette: Union[str, List, Dict, Colormap] = None,
-    style: str = None,
     size: str = None,
-    sizes: Union[Tuple[int, int], Dict, List] = (1, 10),
+    color: str = None,
     label: str = "player",
-    label_alpha: float = 1.0,
-) -> Any:
+    title=None,
+    x_label=None,
+    y_label=None,
+) -> Figure:
     """
     Plot a scatter chart based on `data`, with axes `x` and `y`, including labels on
     the chart based on the column `label`.
     """
 
-    plot = sns.scatterplot(
-        x=x,
-        y=y,
-        hue=hue,
-        palette=palette,
-        style=style,
-        size=size,
-        sizes=sizes,
-        data=data,
+    x_label = x_label or x
+    y_label = y_label or y
+    title = title or f"{x_label} vs {y_label}"
+
+    x_range = (data[x].min(), data[x].max() + 5)
+
+    if size:
+        data["plot_size"] = data[size] / 5
+
+    source = ColumnDataSource(data)
+
+    plot: Figure = figure(
+        plot_width=1000, plot_height=600, title=title, x_range=x_range
     )
 
-    for idx in data.index:
-        xval = data.at[idx, x]
-        yval = data.at[idx, y] + label_alpha
+    plot.xaxis.axis_label = x_label
+    plot.yaxis.axis_label = y_label
 
-        plot.text(
-            xval,
-            yval,
-            data.at[idx, label].split(" ")[-1],
-            horizontalalignment='left',
-            size='x-large',
-            color='black',
+    plot.circle(
+        x=x,
+        y=y,
+        size="plot_size" if size else None,
+        color=color,
+        source=source,
+    )
+
+    if label:
+        plot.add_layout(
+            LabelSet(
+                x=x,
+                y=y,
+                text=label,
+                y_offset=5,
+                source=source,
+                render_mode='canvas',
+                text_font_size={"value": "12px"}
+            )
         )
+
+    plot.add_tools(HoverTool(
+        tooltips=[("Name", f"@{label}")]
+    ))
 
     return plot
