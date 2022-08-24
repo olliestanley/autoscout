764aabf32f796e8be2d2b958ead2153be471bc5e
Oliver Stanley <o.dzineit@gmail.com>
2022-08-24 19:40:56 +0100

    Include function for plotting multiline graphs with trendlines

--- autoscout/vis/chart.py
+++ autoscout/vis/chart.py
@@ -1,7 +1,43 @@
+from typing import Sequence, Tuple, Union
+
+import numpy as np
 import pandas as pd
 from bokeh.models import ColumnDataSource, HoverTool, LabelSet
 from bokeh.plotting import figure
 from bokeh.plotting.figure import Figure
+
+
+def lines(
+    data: pd.DataFrame,
+    x_columns: Sequence[str],
+    y_columns: Sequence[str],
+    colors: Sequence[str],
+    trends: bool = False,
+    title=None,
+    x_label=None,
+    y_label=None,
+    legend_labels: Sequence[str] = None,
+) -> Figure:
+    if legend_labels is None:
+        legend_labels = y_columns
+
+    plot: Figure = figure(
+        plot_width=1000, plot_height=600, title=title,
+        x_axis_label=x_label, y_axis_label=y_label
+    )
+
+    source = ColumnDataSource(data)
+
+    for x, y, col, leg in zip(x_columns, y_columns, colors, legend_labels):
+        plot.line(x, y, color=col, source=source, legend_label=leg)
+
+        if trends:
+            regr = np.polyfit(data[x], data[y], deg=1, full=True)
+            slope, intercept = regr[0][:2]
+            trend = [intercept + slope * v for v in data[x]]
+            plot.line(data[x], trend, color=col, line_dash="dashed")
+
+    return plot
 
 
 def scatter_with_labels(
@@ -26,22 +62,19 @@ def scatter_with_labels(
 
     x_range = (data[x].min(), data[x].max() + 5)
 
+    plot: Figure = figure(
+        plot_width=1000, plot_height=600, title=title, x_range=x_range, x_axis_label=x_label, y_axis_label=y_label
+    )
+
     if size:
         data["plot_size"] = data[size] / 5
 
     source = ColumnDataSource(data)
 
-    plot: Figure = figure(
-        plot_width=1000, plot_height=600, title=title, x_range=x_range
-    )
-
-    plot.xaxis.axis_label = x_label
-    plot.yaxis.axis_label = y_label
-
     plot.circle(
         x=x,
         y=y,
-        size="plot_size" if size else None,
+        size="plot_size" if size else 4,
         color=color,
         source=source,
     )
